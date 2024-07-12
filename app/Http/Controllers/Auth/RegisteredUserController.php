d31e250258f38cfe0be15d08c31f1e652dfe5bd3
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-07-12 11:16:49 +0800

    fix country

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Models\countries;
+use App\Models\Countries;
 use App\Models\Regime;
 use Carbon\Carbon;
 
