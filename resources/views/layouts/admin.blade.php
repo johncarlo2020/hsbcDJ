971cedbeebc5e16bae3aeb24208f483ae68ea948
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-07-17 10:51:55 +0800

    change

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -112,13 +112,7 @@
                 <div class="row align-items-center justify-content-lg-between">
                     <div class="col-lg-6 mb-lg-0 mb-4">
                         <div class="copyright text-center text-sm text-muted text-lg-start">
-                            ©
-                            <script>
-                                document.write(new Date().getFullYear())
-                            </script>,
-                            made with <i class="fa fa-heart"></i> by
                             <a href="https://wowsome.com.my/" class="font-weight-bold" target="_blank">Wowsome</a> © Copyright 2024
-                           
                         </div>
                     </div>
                 </div>
